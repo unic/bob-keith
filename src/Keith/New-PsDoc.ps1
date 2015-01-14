0983ecce167c547a4b431637bb0830946768c7d2
Yannis Guedel <yannis.guedel@unic.com>
2015-01-14 11:48:56 +0100

    update keith docs

--- src/Keith/New-PsDoc.ps1
+++ src/Keith/New-PsDoc.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+Generates a new documentation for a PowerShell module.
+
+.DESCRIPTION
+Generates a new documentation for a PowerShell module,
+by merging the handwritten and the API documentation.
+
+.PARAMETER Path
+The path to the handwritten documentaion.
+
+.PARAMETER OutputLocation
+The path to a folder where the generated documentation should be written.
+
+.PARAMETER Module
+The name of the PowerShell module, for which the API doc should be generated.
+
+.EXAMPLE
+New-PsDoc -Path .\docs -OutputLocation .\docs-generated -Module keith 
+
+#>
 function New-PsDoc
 {
     [CmdletBinding()]
