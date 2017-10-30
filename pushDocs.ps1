958202f5565bbb601bc69423e0d4ace0bde80d32
Reto Hugi <reto.hugi>
2017-10-30 23:55:47 +0100

    Improve pushDocs.ps1 - make git environment more predictable - use variable for gh-pages branch

--- pushDocs.ps1
+++ pushDocs.ps1
@@ -2,9 +2,12 @@ param([string]$buildFolder, [string]$docSource, [string]$repoUrl, [string]$email
 
 git config --global user.email $email
 git config --global user.name $username
-git config --global push.default matching
+git config --global push.default simple
+git config --global core.autocrlf true
+git config --global core.safecrlf false
 
 $workingDir = "$($buildFolder)\..\gh-pages"
+$workingBranch = "gh-pages"
 
 Push-Location
 
@@ -13,7 +16,7 @@ if (Test-Path("$($workingDir)")) {
 }
 mkdir $workingDir
 
-git clone --quiet --branch=gh-pages "$($repoUrl)" "$($workingDir)"
+git clone --quiet --branch=$workingBranch "$($repoUrl)" "$($workingDir)"
 cd $($workingDir)
 git status
 
@@ -28,7 +31,7 @@ if ($thereAreChanges -ne $null) {
     git status
     git commit -m "CI build"
     git status
-    git push --quiet
+    git push
     Write-Host "Changes pushed succesfully" -ForegroundColor Green
 }
 else { 
